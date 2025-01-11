c6eccd789979488d429c0c9b9db0133963c343f3
Shaik Aliya <skaliya205@gmail.com>
2025-01-11 21:49:59 +0530

    initial commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+function Input() {
+  return (
+    <nav>
+      <input
+        name="email"
+        className="inputText"
+        placeholder="Your email address..."
+      />
+      <button className="btn">Notify Me</button>
+    </nav>
+  );
+}
+
+export default Input;
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import mainImg from "imagesillustration-dashboard.png";
+
+function Main() {
+  return (
+    <div className="main-img">
+      <image src={mainImg} alt="illustration" />
+    </div>
+  );
+}
+
+export default Main;
